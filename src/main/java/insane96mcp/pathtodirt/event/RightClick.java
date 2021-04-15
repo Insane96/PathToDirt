b184fe8623dbfaa191ef0b579758f3cebfce19c1
Insane96 <insane96mcp@gmail.com>
2021-04-15 19:51:15 +0200

    Removed commented things

--- src/main/java/insane96mcp/pathtodirt/event/RightClick.java
+++ src/main/java/insane96mcp/pathtodirt/event/RightClick.java
@@ -17,37 +17,6 @@ import java.util.Set;
 @Mod.EventBusSubscriber(modid = PathToDirt.MOD_ID)
 public class RightClick {
 
-	/*@SubscribeEvent
-	public static void eventRightClickBlock(PlayerInteractEvent.RightClickBlock event) {
-		ItemStack holding = event.getItemStack();
-
-		//Check if the item is in the blacklist
-		for (ModConfig.IdTagMatcher idTag : ModConfig.itemBlacklist) {
-			if (idTag.doesItemMatch(holding.getItem()))
-				return;
-		}
-
-		if (!holding.getItem().getToolTypes(holding).contains(ToolType.SHOVEL))
-			return;
-		BlockState state = event.getWorld().getBlockState(event.getPos());
-		ResourceLocation blockToTransformTo = blockToTransformTo(state);
-		if (blockToTransformTo == null)
-			return;
-		if (event.getWorld().isAirBlock(event.getPos().up())) {
-			event.getWorld().playSound(event.getPlayer(), event.getPos(), SoundEvents.ITEM_SHOVEL_FLATTEN, SoundCategory.BLOCKS, 1.0F, 1.0F);
-			event.setResult(Event.Result.ALLOW);
-			event.getWorld().setBlockState(event.getPos(), ForgeRegistries.BLOCKS.getValue(blockToTransformTo).getDefaultState());
-			event.getPlayer().swingArm(event.getHand());
-			if (!event.getWorld().isRemote) {
-				if (!event.getPlayer().isCreative())
-					holding.damageItem(1, event.getPlayer(), player -> player.sendBreakAnimation(event.getHand()));
-			}
-
-			event.setCanceled(true);
-			event.setCancellationResult(ActionResultType.SUCCESS);
-		}
-	}*/
-
 	@SubscribeEvent
 	public static void eventBlockToolInteract(BlockEvent.BlockToolInteractEvent event) {
 		ItemStack holding = event.getHeldItemStack();
@@ -68,18 +37,8 @@ public class RightClick {
 		if (blockToTransformTo == null)
 			return;
 		if (event.getWorld().isAirBlock(event.getPos().up())) {
-			//event.getWorld().playSound(event.getPlayer(), event.getPos(), SoundEvents.ITEM_SHOVEL_FLATTEN, SoundCategory.BLOCKS, 1.0F, 1.0F);
 			event.setResult(Event.Result.ALLOW);
-			//event.getWorld().setBlockState(event.getPos(), ForgeRegistries.BLOCKS.getValue(blockToTransformTo).getDefaultState());
 			event.setFinalState(ForgeRegistries.BLOCKS.getValue(blockToTransformTo).getDefaultState());
-			/*event.getPlayer().swingArm(event.getHand());
-			if (!event.getWorld().isRemote) {
-				if (!event.getPlayer().isCreative())
-					holding.damageItem(1, event.getPlayer(), player -> player.sendBreakAnimation(event.getHand()));
-			}
-
-			event.setCanceled(true);
-			event.setCancellationResult(ActionResultType.SUCCESS);*/
 		}
 	}
 
